1b7f63d70619e3ea4b994ce4e740d47ad87fa5f7
kat-s70 <katsteel@iu.edu>
2022-02-06 00:22:13 -0500

    Complete Exercise

---
+++ Player/Player_Container.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+onready var Player = load("res://Player/Player.tscn")
+
+func _ready():
+	pass
+
+func _physics_process(_delta):
+	if get_child_count() == 0:
+		var player = Player.instance()
+		player.position = Vector2(512,300)
+		add_child(player)
+		
